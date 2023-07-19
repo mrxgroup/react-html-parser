54954addca345fc40bc387c243500cee97db1b17
joey-matchsquare <jbaker@matchsquare.com>
2023-07-19 12:25:35 -0400

    Update dependencies

--- src/index.js
+++ src/index.js
@@ -5,4 +5,5 @@ export { default as processNodes } from './processNodes';
 export { default as convertNodeToElement } from './convertNodeToElement';
 
 // expose htmlparser2 so it can be used if required
-export { default as htmlparser2 } from 'htmlparser2';
+import * as htmlparser2 from 'htmlparser2';
+export { htmlparser2 };
